8abe83ac10dd4257024fe7e4381c418ecf98fc28
LuoWei <1092338140@qq.com>
2019-10-23 20:10:47 +0800

    Initial commit

---
+++ src/main/java/com/qf/MyTest.java
@@ -0,0 +1,11 @@
+package com.qf;
+
+/**
+ * @author LuoWei
+ * @Date 2019/10/23
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("从IDEA提交到github");
+    }
+}
