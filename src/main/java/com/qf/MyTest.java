820bc638fdaa5eac42b1d4e45295ac43fe3c3821
LuoWei <1092338140@qq.com>
2019-10-23 20:22:48 +0800

    重新写了个

--- src/main/java/com/qf/MyTest.java
+++ src/main/java/com/qf/MyTest.java
@@ -8,6 +8,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("从IDEA提交到github");
         System.out.println("github修改供idea下载---hahahaha");
-        
+        System.out.println("3333");
     }
 }
